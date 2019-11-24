8c0554345711881f1356aa772313ad2cd86699bd
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-11-24 09:41:54 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -4,10 +4,21 @@ class YouTubeDebugger extends React.Component{
   constructor(){
     super()
     this.state = {
-      
+        errors: [],
+        user: null,
+        settings: {
+        bitrate: 8,
+        video: {
+      resolution: '1080p'
+         }
+      }
     }
   }
   render(){
-    
+    return(
+      <div>
+      <button className=>
+      </div>
+      )
   }
 }
