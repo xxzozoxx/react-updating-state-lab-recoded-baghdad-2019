fe9d5a9fd8ed00b1151ad77d0359b297022dc28a
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-11-24 09:26:34 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,15 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+class DigitalClicker extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      timesClicked:0
+    }
+  }
+  render(){
+    return (
+      <button label={this.state.timesClicked}>{label} </button>
+      )
+  }
+}
